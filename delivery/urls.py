c0ed1b25eb0740231ff7c5807d9b921afd4a0451
LeeRude11 <hov3r@yandex.ru>
2019-01-22 17:10:11 +0500

    Plug in accounts app

--- delivery/urls.py
+++ delivery/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('menu/', include('menu.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls)
 ]
