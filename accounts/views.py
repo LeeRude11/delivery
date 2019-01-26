5a2fcfadf05f0b102426e4db73184be8637bc9c7
LeeRude11 <hov3r@yandex.ru>
2019-01-26 13:18:02 +0500

    Base ProfileView, CustomPasswordChangeView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import base
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class ProfileView(base.TemplateView):
+    template_name = 'registration/profile.html'
+
+    # TODO login_required
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['username'] = user.username
+        return context
+
+
+class CustomPasswordChangeView(auth_views.PasswordChangeView):
+    success_url = reverse_lazy('accounts:profile')
