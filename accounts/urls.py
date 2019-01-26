2c57ef10043cc6ffb3d6e81d9dc58efe8ac30efb
LeeRude11 <hov3r@yandex.ru>
2019-01-26 15:02:35 +0500

    register/

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import CustomPasswordChangeView, ProfileView
+from .views import CustomPasswordChangeView, ProfileView, RegisterView
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.logout_then_login, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('password_change/', CustomPasswordChangeView.as_view(),
          name='password_change'),
-    path('profile/', ProfileView.as_view(), name='profile')
 ]
