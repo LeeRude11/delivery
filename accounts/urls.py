92cb845eea9d0c10b20f9a5038250937dd4b539b
LeeRude11 <hov3r@yandex.ru>
2019-01-26 13:16:52 +0500

    login, password_change, profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import CustomPasswordChangeView, ProfileView
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('password_change/', CustomPasswordChangeView.as_view(),
+         name='password_change'),
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
