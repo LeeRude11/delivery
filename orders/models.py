5f528f6acee7132bd9e9df8d3d05e90b3c9ba7bd
LeeRude11 <hov3r@yandex.ru>
2019-01-27 12:13:02 +0500

    Mock models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class OrderInfo(models.Model):
+    # TODO: users from accounts app
+    foreign_user_id = None
+    # TODO: a function to process OrderContents and return cost
+    cost = models.IntegerField
+    # TODO: a function to process OrderLog and return current status
+    status = None
+
+
+class OrderContents(models.Model):
+    # TODO: plug OrderInfo
+    foreign_order_id = None
+    # TODO: menu items from menu app
+    foreign_menu_item_id = None
+    amount = models.IntegerField
+
+
+class OrderLog(models.Model):
+    # TODO: plug OrderInfo
+    foreign_order_id = None
+    # TODO: datetime format
+    ordered = None
+    cooked = None
+    delivered = None
