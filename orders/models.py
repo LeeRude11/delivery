c08082c652fc66990fcce6f0e2871276a17b698b
LeeRude11 <hov3r@yandex.ru>
2019-01-28 15:54:20 +0500

    Base OrderContents

--- orders/models.py
+++ orders/models.py
@@ -1,21 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class OrderInfo(models.Model):
-    # TODO: users from accounts app
-    foreign_user_id = None
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # TODO: a function to process OrderContents and return cost
-    cost = models.IntegerField
+    cost = models.IntegerField(default=1)
     # TODO: a function to process OrderLog and return current status
     status = None
 
 
 class OrderContents(models.Model):
-    # TODO: plug OrderInfo
-    foreign_order_id = None
-    # TODO: menu items from menu app
-    foreign_menu_item_id = None
-    amount = models.IntegerField
+    order = models.ForeignKey(OrderInfo, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey('menu.MenuItem', on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
 
 
 class OrderLog(models.Model):
