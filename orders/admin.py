2e780f764e11d20219f429056a2fc6bc7d1825c8
LeeRude11 <hov3r@yandex.ru>
2019-01-30 13:41:24 +0500

    Base interface

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import OrderInfo, OrderContents
+
+
+class OrderContentsInline(admin.TabularInline):
+    model = OrderContents
+    extra = 0
+
+
+class OrderInfoAdmin(admin.ModelAdmin):
+    inlines = [OrderContentsInline]
+    list_display = ('id', 'ordered', 'cooked', 'delivered', 'cost')
+
+
+admin.site.register(OrderInfo, OrderInfoAdmin)
