6fd10c0680bca984de21c194c5d5e31363cc9e90
LeeRude11 <hov3r@yandex.ru>
2019-02-02 13:12:05 +0500

    Filter orders by delivered or not

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
 from .models import OrderInfo, OrderContents
 
@@ -8,9 +9,32 @@ class OrderContentsInline(admin.TabularInline):
     extra = 0
 
 
+class DeliveredListFilter(admin.SimpleListFilter):
+
+    title = _('Is delivered')
+
+    parameter_name = 'is delivered'
+
+    def lookups(self, request, model_admin):
+
+        return (
+            ('yes', _('Yes')),
+            ('no',  _('No')),
+        )
+
+    def queryset(self, request, queryset):
+
+        if self.value() == 'yes':
+            return queryset.filter(delivered__isnull=False)
+
+        if self.value() == 'no':
+            return queryset.filter(delivered__isnull=True)
+
+
 class OrderInfoAdmin(admin.ModelAdmin):
     inlines = [OrderContentsInline]
     list_display = ('id', 'ordered', 'cooked', 'delivered', 'total_cost')
+    list_filter = [DeliveredListFilter]
 
 
 admin.site.register(OrderInfo, OrderInfoAdmin)
