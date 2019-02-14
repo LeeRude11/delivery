55922e5031cc52e0dd3a27e4dd20366cb87aaef5
LeeRude11 <hov3r@yandex.ru>
2019-02-14 11:22:41 +0500

    add_to_cart url

--- menu/urls.py
+++ menu/urls.py
@@ -6,4 +6,6 @@ app_name = 'menu'
 urlpatterns = [
     path('', views.MenuListView.as_view(), name='menu'),
     path('<int:pk>/', views.MenuItemView.as_view(), name='detail'),
+    path('<int:menuitem_id>/add_to_cart',
+         views.add_to_cart, name='add_to_cart')
 ]
