2a09d65e736fc1f57f9c65ac58d6eb1192ea0b19
LeeRude11 <hov3r@yandex.ru>
2019-02-14 15:47:47 +0500

    shopping_cart(), build_cart_context()

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,31 @@
-from django.views import generic
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 
-from .models import OrderInfo
+from menu.models import MenuItem
 
 
-class ShoppingCartView(generic.ListView):
+def shopping_cart(request):
+    if request.user.is_authenticated is False:
+        messages.error(request, "Must be logged in.")
+        return HttpResponseRedirect(reverse('accounts:login'))
     template_name = 'orders/shopping_cart.html'
-    model = OrderInfo
+    cart_context = build_cart_context(request.session.get('cart', {}))
+    return render(request, template_name, cart_context)
+
+
+def build_cart_context(cart_dict):
+    cart_cost = 0
+    contents = []
+    for item, amount in cart_dict.items():
+        current_item = MenuItem.objects.get(pk=item)
+        cost = current_item.price * int(amount)
+        contents.append({
+            'name': current_item.name,
+            'price': current_item.price,
+            'amount': int(amount),
+            'cost': cost
+        })
+        cart_cost += cost
+    return {'cart_cost': cart_cost, 'contents': contents}
