1515fa67b65fb1b2e4766d79239e1d40bdfe8090
LeeRude11 <hov3r@yandex.ru>
2019-02-15 11:40:43 +0500

    Tests for process_order()

--- orders/tests.py
+++ orders/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
 
 from random import randint
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from .models import OrderInfo, OrderContents
 from menu.models import MenuItem
@@ -139,3 +140,80 @@ class ShoppingCartViewTests(TestCase):
 
         response = self.client.get(url)
         self.assertEqual(response.context['cart_cost'], expected_cart_cost)
+
+    def test_process_order_redirects_anon(self):
+        """
+        TEMPORARY - Process order redirects unauthorized users to login.
+        """
+        url = reverse('orders:process_order')
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response, reverse('accounts:login'))
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, 'error')
+        self.assertTrue("Must be logged in." in message.message)
+
+    def test_process_order_redirects_get(self):
+        """
+        Process order is only available with POST.
+        """
+        User.objects.create_user('testuser', None, 'testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('orders:process_order')
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response, reverse('orders:shopping_cart'))
+
+    def test_process_empty_cart(self):
+        """
+        Trying to order an empty cart will redirect with error message.
+        """
+        User.objects.create_user('testuser', None, 'testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('orders:process_order')
+        response = self.client.post(url, follow=True)
+
+        self.assertRedirects(response, reverse('orders:shopping_cart'))
+        message = list(response.context.get('messages'))[0]
+        self.assertEqual(message.tags, 'error')
+        self.assertTrue("Your cart is empty." in message.message)
+
+    def test_process_order(self):
+        """
+        Order is successfully transefrered from Session to database.
+        """
+        User.objects.create_user('testuser', None, 'testpassword')
+        self.client.login(username='testuser', password='testpassword')
+
+        new_menu_items_list = [
+            create_menu_item(name=f'Dish{i}') for i in range(3)]
+        expected_contents = []
+        for new_menu_item in new_menu_items_list:
+            add_url = reverse('menu:add_to_cart', args=(new_menu_item.id,))
+            amount = randint(1, 10)
+            self.client.post(add_url, {'amount': amount})
+            expected_contents.append({
+                'name': new_menu_item.name,
+                'price': new_menu_item.price,
+                'amount': amount,
+                'cost': new_menu_item.price * amount
+            })
+
+        url = reverse('orders:process_order')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your order was placed.")
+
+        placed_order = OrderInfo.objects.get(pk=1)
+        order_contents = placed_order.ordercontents_set.all()
+        # arbitrary 5 seconds to account for some fault
+        self.assertTrue(
+            timezone.now() - placed_order.ordered < timedelta(seconds=5))
+        self.assertEqual(len(expected_contents), len(order_contents))
+        for expected in expected_contents:
+            db_contents = order_contents.get(menu_item__name=expected['name'])
+            dict_from_db = {
+                'name': db_contents.menu_item.name,
+                'price': db_contents.menu_item.price,
+                'amount': db_contents.amount,
+                'cost': db_contents.cost,
+            }
+            self.assertEqual(expected, dict_from_db)
