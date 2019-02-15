ac1135d41a1c77369924ec8c7aad68cf7861b082
LeeRude11 <hov3r@yandex.ru>
2019-02-15 11:40:01 +0500

    process_order url

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'orders'
 urlpatterns = [
-    path('shopping_cart/', views.shopping_cart, name='shopping_cart')
+    path('shopping_cart/', views.shopping_cart, name='shopping_cart'),
+    path('process_order/', views.process_order, name='process_order')
 ]
