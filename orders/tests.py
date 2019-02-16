c9665bb35b0abd042ba8bc332b14e2d1ca8a76db
LeeRude11 <hov3r@yandex.ru>
2019-02-16 10:37:26 +0500

    Some refactoring

--- orders/tests.py
+++ orders/tests.py
@@ -72,41 +72,43 @@ class OrderInfoTests(TestCase):
 
 class ShoppingCartViewTests(TestCase):
 
-    def test_shoping_cart_redirects_anon_to_login(self):
+    def assert_redirect_and_error_msg(
+            self, path, redirect_to='accounts:login', get=True,
+            msg_text="Must be logged in."):
         """
-        TEMPORARY - unauthorized users can not access shopping cart page.
+        Page redirects and shows an error message.
+        Default is for a users-only page.
+        Only accepts GET and POST requests
         """
-        url = reverse('orders:shopping_cart')
-        response = self.client.get(url, follow=True)
-        self.assertRedirects(response, reverse('accounts:login'))
+        url = reverse(path)
+        if get:
+            response = self.client.get(url, follow=True)
+        else:
+            response = self.client.post(url, follow=True)
+        self.assertRedirects(response, reverse(redirect_to))
         message = list(response.context.get('messages'))[0]
         self.assertEqual(message.tags, 'error')
-        self.assertTrue("Must be logged in." in message.message)
+        self.assertTrue(msg_text in message.message)
 
-    def test_shoping_cart_is_empty(self):
+    def login_test_user(self):
         """
-        Opening page with empty cart displays appropriate text.
+        Create and login a test user.
         """
         User.objects.create_user('testuser', None, 'testpassword')
         self.client.login(username='testuser', password='testpassword')
-        url = reverse('orders:shopping_cart')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Your shopping cart is empty.")
-        self.assertQuerysetEqual(response.context['contents'], [])
 
-    def test_shoping_cart_not_empty(self):
+    def user_access_url(self, path):
         """
-        Page has user's cart contents.
+        Create and login a test user, then access passed path.
         """
-        User.objects.create_user('testuser', None, 'testpassword')
-        self.client.login(username='testuser', password='testpassword')
-        url = reverse('orders:shopping_cart')
+        self.login_test_user()
+        return reverse(path)
 
-        new_menu_items_list = [
-            create_menu_item(name=f'Dish{i}') for i in range(3)]
+    def fill_session_cart(self):
         expected_contents = []
-        for new_menu_item in new_menu_items_list:
+        for i in range(3):
+            new_menu_item = create_menu_item(
+                name=f'Dish{i}', price=randint(10, 300))
             add_url = reverse('menu:add_to_cart', args=(new_menu_item.id,))
             amount = randint(1, 10)
             self.client.post(add_url, {'amount': amount})
@@ -116,27 +118,43 @@ class ShoppingCartViewTests(TestCase):
                 'amount': amount,
                 'cost': new_menu_item.price * amount
             })
+        return expected_contents
 
+    def test_shopping_cart_redirects_anon_to_login(self):
+        """
+        TEMPORARY - unauthorized users can not access shopping cart page.
+        """
+        self.assert_redirect_and_error_msg(path='orders:shopping_cart')
+
+    def test_shopping_cart_is_empty(self):
+        """
+        Opening page with empty cart displays appropriate text.
+        """
+        url = self.user_access_url('orders:shopping_cart')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your shopping cart is empty.")
+        self.assertQuerysetEqual(response.context['contents'], [])
+
+    def test_shopping_cart_not_empty(self):
+        """
+        Page has user's cart contents.
+        """
+        url = self.user_access_url('orders:shopping_cart')
+
+        expected_contents = self.fill_session_cart()
         response = self.client.get(url)
         self.assertEqual(response.context['contents'], expected_contents)
 
-    def test_shoping_cart_displays_total_cost(self):
+    def test_shopping_cart_displays_total_cost(self):
         """
         Whole cart cost is correctly calculated and displayed.
         """
-        User.objects.create_user('testuser', None, 'testpassword')
-        self.client.login(username='testuser', password='testpassword')
-        url = reverse('orders:shopping_cart')
+        url = self.user_access_url('orders:shopping_cart')
 
-        new_menu_items_list = [
-            create_menu_item(
-                name=f'Dish{i}', price=randint(10, 300)) for i in range(3)]
         expected_cart_cost = 0
-        for new_menu_item in new_menu_items_list:
-            add_url = reverse('menu:add_to_cart', args=(new_menu_item.id,))
-            amount = randint(1, 10)
-            expected_cart_cost += amount * new_menu_item.price
-            self.client.post(add_url, {'amount': amount})
+        for item in self.fill_session_cart():
+            expected_cart_cost += item['price'] * item['amount']
 
         response = self.client.get(url)
         self.assertEqual(response.context['cart_cost'], expected_cart_cost)
@@ -145,20 +163,13 @@ class ShoppingCartViewTests(TestCase):
         """
         TEMPORARY - Process order redirects unauthorized users to login.
         """
-        url = reverse('orders:process_order')
-        response = self.client.get(url, follow=True)
-        self.assertRedirects(response, reverse('accounts:login'))
-        message = list(response.context.get('messages'))[0]
-        self.assertEqual(message.tags, 'error')
-        self.assertTrue("Must be logged in." in message.message)
+        self.assert_redirect_and_error_msg(path='orders:process_order')
 
     def test_process_order_redirects_get(self):
         """
         Process order is only available with POST.
         """
-        User.objects.create_user('testuser', None, 'testpassword')
-        self.client.login(username='testuser', password='testpassword')
-        url = reverse('orders:process_order')
+        url = self.user_access_url('orders:process_order')
         response = self.client.get(url, follow=True)
         self.assertRedirects(response, reverse('orders:shopping_cart'))
 
@@ -166,38 +177,20 @@ class ShoppingCartViewTests(TestCase):
         """
         Trying to order an empty cart will redirect with error message.
         """
-        User.objects.create_user('testuser', None, 'testpassword')
-        self.client.login(username='testuser', password='testpassword')
-        url = reverse('orders:process_order')
-        response = self.client.post(url, follow=True)
+        self.login_test_user()
 
-        self.assertRedirects(response, reverse('orders:shopping_cart'))
-        message = list(response.context.get('messages'))[0]
-        self.assertEqual(message.tags, 'error')
-        self.assertTrue("Your cart is empty." in message.message)
+        self.assert_redirect_and_error_msg(
+            'orders:process_order', redirect_to='orders:shopping_cart',
+            get=False, msg_text="Your cart is empty.")
 
     def test_process_order(self):
         """
         Order is successfully transefrered from Session to database.
         """
-        User.objects.create_user('testuser', None, 'testpassword')
-        self.client.login(username='testuser', password='testpassword')
+        url = self.user_access_url('orders:process_order')
 
-        new_menu_items_list = [
-            create_menu_item(name=f'Dish{i}') for i in range(3)]
-        expected_contents = []
-        for new_menu_item in new_menu_items_list:
-            add_url = reverse('menu:add_to_cart', args=(new_menu_item.id,))
-            amount = randint(1, 10)
-            self.client.post(add_url, {'amount': amount})
-            expected_contents.append({
-                'name': new_menu_item.name,
-                'price': new_menu_item.price,
-                'amount': amount,
-                'cost': new_menu_item.price * amount
-            })
+        expected_contents = self.fill_session_cart()
 
-        url = reverse('orders:process_order')
         response = self.client.post(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Your order was placed.")
@@ -209,6 +202,7 @@ class ShoppingCartViewTests(TestCase):
             timezone.now() - placed_order.ordered < timedelta(seconds=5))
         self.assertEqual(len(expected_contents), len(order_contents))
         for expected in expected_contents:
+            # .get also raises exception if more or less than 1 match found
             db_contents = order_contents.get(menu_item__name=expected['name'])
             dict_from_db = {
                 'name': db_contents.menu_item.name,
