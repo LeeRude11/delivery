b2db8a4c97aa0c9d3711344a120077ddba132fec
LeeRude11 <hov3r@yandex.ru>
2019-02-17 16:05:29 +0500

    orders_update_cart url

--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('shopping_cart/', views.shopping_cart, name='shopping_cart'),
+    path('<int:menuitem_id>/update_cart', views.orders_update_cart,
+         name='orders_update_cart'),
     path('process_order/', views.process_order, name='process_order')
 ]
