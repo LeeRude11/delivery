1a55c4f54213feeff0b6845f140d05bad6924958
LeeRude11 <hov3r@yandex.ru>
2019-02-23 09:17:26 +0500

    TODO

---
+++ accounts/tests_selenium.py
@@ -0,0 +1,16 @@
+from menu.tests_selenuim import FirefoxTests
+
+
+class RegisterTests(FirefoxTests):
+
+    pass
+
+
+class LoginTests(FirefoxTests):
+
+    pass
+
+
+class ProfileTests(FirefoxTests):
+
+    pass
