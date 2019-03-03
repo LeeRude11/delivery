72017c9911dbc7c72e5b97f3f27ef236e2e03c07
LeeRude11 <hov3r@yandex.ru>
2019-03-03 14:31:14 +0500

    Custom User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = (
+        'phone_number',
+        'first_name',
+        'second_name',
+        'email',
+        'date_of_birth',
+        'street',
+        'house',
+        'apartment',
+        'date_of_birth',
+        'is_admin'
+    )
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Personal info', {
+            'fields': ('first_name', 'second_name', 'email', 'date_of_birth',)
+            }),
+        ('Address', {
+            'fields': ('street', 'house', 'apartment',)
+            }),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'phone_number',
+                'password1',
+                'password2',
+                'first_name',
+                'second_name',
+                'email',
+                'date_of_birth',
+                'street',
+                'house',
+                'apartment',
+                'date_of_birth',
+            )
+        }),
+    )
+    search_fields = ('phone_number',)
+    ordering = ('phone_number',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,127 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(
+            self, phone_number, password,
+            first_name, second_name,
+            street, house, apartment='',
+            email=None, date_of_birth=None
+            ):
+        """
+        Creates and saves a User with the given phone number, password,
+        full address and optional email and date of birth.
+        """
+        if not phone_number:
+            raise ValueError('Users must have a phone number')
+
+        # TODO standardize phone number
+        # TODO should all required fields raise an error when missing?
+        user = self.model(
+            phone_number=phone_number,
+            first_name=first_name,
+            second_name=second_name,
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            street=street,
+            house=house,
+            apartment=apartment
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+            self, phone_number, password,
+            first_name, second_name,
+            street, house, apartment='',
+            email=None, date_of_birth=None
+            ):
+        """
+        Creates and saves a superuser with the given phone number, password,
+        full address and optional email and date of birth.
+        """
+        # TODO superuser-s might need an email
+        # TODO create a superuser without create_user fields(address)?
+        user = self.create_user(
+            phone_number=phone_number,
+            password=password,
+            first_name=first_name,
+            second_name=second_name,
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            street=street,
+            house=house,
+            apartment=apartment
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    # TODO max length etc.
+    phone_number = models.CharField(
+        max_length=64,
+        unique=True
+    )
+    first_name = models.CharField(max_length=64)
+    second_name = models.CharField(max_length=64)
+    street = models.CharField(
+        max_length=64,
+        verbose_name='street name'
+    )
+    house = models.CharField(
+        max_length=64,
+        verbose_name='house number'
+    )
+    apartment = models.CharField(
+        max_length=64,
+        verbose_name='apartment, suite or room number',
+        blank=True
+    )
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+        blank=True,
+        null=True
+    )
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = [
+        'first_name',
+        'second_name',
+        'street',
+        'house',
+    ]
+
+    def __str__(self):
+        return self.phone_number
+
+    # TODO permissions
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
