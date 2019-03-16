2b843c80843189b8ab4f83d77c5049207282e607
LeeRude11 <hov3r@yandex.ru>
2019-03-16 13:17:22 +0500

    class-based to function-based /register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (
-    CustomPasswordChangeView, ProfileView, RegisterView, CustomLoginView)
+    CustomPasswordChangeView, ProfileView, CustomLoginView, registration)
 
 app_name = 'accounts'
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.logout_then_login, name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', registration, name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('password_change/', CustomPasswordChangeView.as_view(),
          name='password_change'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,17 @@
 from django.views.generic import base, edit
 from django.contrib import auth
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
 from .forms import UserCreationForm, CustomAuthForm
+SUCCESS_REG_REDIRECT = 'accounts:profile'
 
 
-class ProfileView(base.TemplateView):
+class ProfileView(LoginRequiredMixin, base.TemplateView):
     template_name = 'registration/profile.html'
 
-    # TODO login_required
     def get_context_data(self, **kwargs):
         # TODO a lot more information
         context = super().get_context_data(**kwargs)
@@ -26,6 +29,21 @@ class RegisterView(edit.CreateView):
     form_class = UserCreationForm
     # TODO login automatically
     success_url = reverse_lazy('accounts:login')
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('phone_number')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(SUCCESS_REG_REDIRECT)
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 
 class CustomLoginView(auth.views.LoginView):
