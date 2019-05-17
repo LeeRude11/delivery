8bcf28e0e9617294bba1731ca20856fbb9d0c463
LeeRude11 <hov3r@yandex.ru>
2019-05-17 21:37:05 +0500

    some base root app views

---
+++ delivery/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+def index(request):
+    """
+    Index page.
+    """
+    template_name = 'index.html'
+    return render(request, template_name)
+
+
+def delivery(request):
+    """
+    Info about delivery services.
+    """
+    template_name = 'delivery.html'
+    return render(request, template_name)
+
+
+def info(request):
+    """
+    Information about the company.
+    """
+    template_name = 'info.html'
+    return render(request, template_name)
