4a313122b1f7d3c6002227abebc5f9a2c66d21cc
LeeRude11 <hov3r@yandex.ru>
2019-06-03 20:20:27 +0500

    update_cart without menuitem_id in path

--- menu/urls.py
+++ menu/urls.py
@@ -6,6 +6,5 @@ app_name = 'menu'
 urlpatterns = [
     path('', views.MenuListView.as_view(), name='menu'),
     path('<int:pk>/', views.MenuItemView.as_view(), name='detail'),
-    path('<int:menuitem_id>/update_cart',
-         views.update_cart, name='update_cart')
+    path('update_cart/', views.update_cart, name='update_cart')
 ]
