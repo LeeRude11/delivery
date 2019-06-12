3157b5336d217dba01ebdf8be90ea0d6a336e34b
LeeRude11 <hov3r@yandex.ru>
2019-06-12 22:26:14 +0500

    ImageField with default

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+# TODO actual img
+DEFAULT_ITEM_IMG = 'menu_items/default.png'
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=64, unique=True)
     price = models.IntegerField(validators=[MinValueValidator(1)])
+    # TODO - height and width fields
+    image = models.ImageField(
+        upload_to='menu_items/',
+        default=DEFAULT_ITEM_IMG)
 
     def __str__(self):
         return self.name
