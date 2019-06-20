9941f994de6b995e1c03ba6e4ef000008da88f1f
LeeRude11 <hov3r@yandex.ru>
2019-06-20 22:16:10 +0500

    info_views_processor -> navbar_ ; .active class for rendering those

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,30 @@
+from django.urls import reverse
+
 from .models import InfoViewTemplate
 
 
 def info_views_processor(request):
     info_views = InfoViewTemplate.objects.all()
     return {'info_views': info_views}
+
+
+def navbar_views_processor(request):
+    info_views = InfoViewTemplate.objects.all()
+
+    menu_views = [
+        {
+            'title': 'Specials',
+            'url': reverse('menu:specials')
+        },
+        {
+            'title': 'Menu',
+            'url': reverse('menu:menu')
+        },
+    ]
+
+    for view in info_views:
+        menu_views.append({
+            'title': view.title,
+            'url': reverse('core:info', args=(view.view_name,))
+        })
+    return {'navbar_views': menu_views}
--- delivery/settings.py
+++ delivery/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'core.context_processors.info_views_processor',
+                'core.context_processors.navbar_views_processor',
             ],
         },
     },
