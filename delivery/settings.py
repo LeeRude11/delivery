27cf060df2da027e62c9c6bfb7cc906ed9b94e47
LeeRude11 <hov3r@yandex.ru>
2019-06-24 21:31:49 +0500

    heroku to ALLOWED_HOSTS

--- delivery/settings.py
+++ delivery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Not really that secret; TODO'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
