1e50c25a72dc5f45bcc639668ddc205004a7090e
LeeRude11 <hov3r@yandex.ru>
2019-06-26 13:10:33 +0500

    remove SECRET_KEY for good

--- delivery/settings.py
+++ delivery/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o1oq!!q1$t&7oi3($bkyr!+s!f+vku^2(6gc9$x2)7go=--&)o'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
