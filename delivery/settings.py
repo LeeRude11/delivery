c9fa314d5bf1e46fe64d00392d4614dad2919527
LeeRude11 <hov3r@yandex.ru>
2019-07-06 17:06:31 +0500

    DEBUG env

--- delivery/settings.py
+++ delivery/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DJANGO_DEBUG') == '1':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
