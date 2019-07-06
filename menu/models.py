ca4863aa3c79595a78ef3428a70f5f1b0e0919cc
LeeRude11 <hov3r@yandex.ru>
2019-07-06 12:28:28 +0500

    Placeholder MenuSpecial model

--- menu/models.py
+++ menu/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinValueValidator
 
 # TODO actual img
 DEFAULT_ITEM_IMG = 'menu_items/default.png'
+DEFAULT_SPECIAL_IMG = 'menu_specials/default.png'
 
 
 class MenuItem(models.Model):
@@ -15,3 +16,14 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class MenuSpecial(models.Model):
+    """
+    Placeholder model.
+    """
+    name = models.CharField(max_length=64, unique=True)
+    image = models.ImageField(
+        upload_to='menu_specials/',
+        default=DEFAULT_SPECIAL_IMG)
+    available = models.BooleanField(default=True)
